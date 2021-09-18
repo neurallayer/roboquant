aa8f602ccd0f5363ed0cb05034977d3e2a0a5dd0
peter <peter@jbaron.com>
2021-09-18 12:55:17 +0200

    updated dokka

--- roboquant-core/src/common/Config.kt
+++ roboquant-core/src/common/Config.kt
@@ -37,7 +37,6 @@ object Config {
         val osInfo = System.getProperty("os.name") + " " + System.getProperty("os.version")
         val runtimeInfo = Runtime.getRuntime().maxMemory() / (1024 * 1024)
 
-
         val msg = """             _______ 
             | $   $ |   roboquant v$version  home=$home
             |   o   |   $osInfo
@@ -74,10 +73,10 @@ object Config {
     /**
      * Get property value. Tries to find the property in this order
      *
-     *  1. system properties
-     *  2. System environment variables
-     *  3. ".env" file in working directory
-     *  4. ".env" file in roboquant home directory (normally $USER/.roboquant)
+     *  1. System properties (`java -DpropertyName=value`)
+     *  2. Environment variables set by the OS
+     *  3. "dotenv" or ".env" property file in the current working directory
+     *  4. ".env" property file in roboquant home directory (normally $USER/.roboquant)
      *
      * If nothing is found, it will return the default value or if that is not provided null
      *
@@ -89,18 +88,20 @@ object Config {
     }
 
     /**
-     * load properties from an environment file.
+     * load properties from an environment file. We don't use lazy so any change to the file is picked-up
+     * immediately.
      */
-    private val env by lazy {
-        val prop = Properties()
-        fun load(path: Path) {
-            if (Files.exists(path)) prop.load(path.toFile().inputStream())
-        }
+    private val env : Map<String, String>
+        get() {
+            val prop = Properties()
+            fun load(path: Path) {
+                if (Files.exists(path)) prop.load(path.toFile().inputStream())
+            }
 
-        load(home / ".env")
-        load(Path.of(".env"))
-        load(Path.of("dotenv"))
-        prop.map { it.key.toString() to it.value.toString() }.toMap()
+            load(home / ".env")
+            load(Path.of(".env"))
+            load(Path.of("dotenv"))
+            return prop.map { it.key.toString() to it.value.toString() }.toMap()
     }
 
 }
