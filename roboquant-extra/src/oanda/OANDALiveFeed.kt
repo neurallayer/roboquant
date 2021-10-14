af89df6864a2e75946c9c7ebacfd21fe20f80907
peter <peter@jbaron.com>
2021-10-14 23:20:41 +0200

    Added Apache license header to all source files.

--- roboquant-extra/src/oanda/OANDALiveFeed.kt
+++ roboquant-extra/src/oanda/OANDALiveFeed.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Neural Layer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.roboquant.oanda
 
 import com.google.gson.JsonParser
@@ -6,7 +22,6 @@ import com.oanda.v20.account.AccountID
 import com.oanda.v20.pricing.PricingGetRequest
 import kotlinx.coroutines.delay
 import oanda.OANDAConnection
-import okhttp3.internal.wait
 import org.apache.http.client.methods.CloseableHttpResponse
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.HttpClientBuilder
