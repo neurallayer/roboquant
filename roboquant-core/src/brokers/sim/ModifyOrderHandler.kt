e491f4720596fd0a161d8ad651adb7d3f1d8b801
Peter Dekkers <peter@jbaron.com>
2022-02-26 00:04:30 +0100

    Simpler update orders

--- roboquant-core/src/brokers/sim/ModifyOrderHandler.kt
+++ roboquant-core/src/brokers/sim/ModifyOrderHandler.kt
@@ -3,13 +3,16 @@ package org.roboquant.brokers.sim
 import org.roboquant.orders.*
 import java.time.Instant
 
-internal class UpdateOrderHandler(order: UpdateOrder, private val handlers: List<OrderHandler<*>>) : OrderHandler<UpdateOrder>(order) {
+internal class UpdateOrderHandler(order: UpdateOrder, private val handlers: List<OrderHandler<*>>) :
+    OrderHandler<UpdateOrder>(order) {
 
     override fun execute(pricing: Pricing, time: Instant): List<Execution> {
         update(time)
-        val handler = handlers.filterIsInstance<OrderHandler<SingleOrder>>().firstOrNull { it.order.id == order.original.id }
+        val handler = handlers.filterIsInstance<OrderHandler<SingleOrder>>().firstOrNull {
+            it.order.id == order.original.id
+        }
         if (handler != null && handler.status.open) {
-           handler.order = order.update
+            handler.order = order.update
             close(OrderStatus.COMPLETED, time)
         } else {
             close(OrderStatus.REJECTED, time)
@@ -20,7 +23,8 @@ internal class UpdateOrderHandler(order: UpdateOrder, private val handlers: List
 }
 
 
-internal class CancelOrderHandler(order: CancelOrder, private val handlers: List<OrderHandler<*>>) : OrderHandler<CancelOrder>(order) {
+internal class CancelOrderHandler(order: CancelOrder, private val handlers: List<OrderHandler<*>>) :
+    OrderHandler<CancelOrder>(order) {
 
     override fun execute(pricing: Pricing, time: Instant): List<Execution> {
         update(time)
