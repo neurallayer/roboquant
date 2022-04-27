c448e4add0a0bf400fab6e735b456fc5c6a6a24f
Peter Dekkers <peter@jbaron.com>
2022-04-27 18:23:57 +0200

    Refactored some code to improve maintainability

--- roboquant-ta/src/ta/Ta4jStrategy.kt
+++ roboquant-ta/src/ta/Ta4jStrategy.kt
@@ -17,10 +17,10 @@ import org.ta4j.core.rules.BooleanRule
  * custom strategy. Ta4j is a flexible framework written in Java that allows to combine indicators and rules and comes
  * with over 130 indicators.
  *
- * @property buyingRule function that returns a buying rule based on the provided BarSaries
- * @property sellingRule function that returns a selling rule based on the provided BarSaries
- * @property maxBarCount maximum number of pricebars to track, default is -1 meaining all
- * @constructor Create new ta4j strategy
+ * @property buyingRule function that returns a buying rule based on the provided BarSaries, default is always FALSE
+ * @property sellingRule function that returns a selling rule based on the provided BarSaries, default is always FALSE
+ * @property maxBarCount maximum number of pricebars to track, default is -1 meaning track all bars
+ * @constructor Create a new ta4j strategy
  */
 class Ta4jStrategy(
     private val maxBarCount:Int = -1,
@@ -28,21 +28,24 @@ class Ta4jStrategy(
 
     private var buyingRule: (BarSeries) -> Rule = { BooleanRule.FALSE }
     private var sellingRule: (BarSeries) -> Rule = { BooleanRule.FALSE }
-    private val data = mutableMapOf<Asset, Triple<Rule, Rule, BarSeries>>()
+    private val rules = mutableMapOf<Asset, Triple<Rule, Rule, BarSeries>>()
+
+    private fun getRules(asset: Asset): Triple<Rule, Rule, BarSeries> {
+        return rules.getOrPut(asset) {
+            val series = BaseBarSeriesBuilder().withName(asset.symbol).build()
+            if (maxBarCount >= 0) series.maximumBarCount = maxBarCount
+            val rule1 = buyingRule(series)
+            val rule2 = sellingRule(series)
+            Triple(rule1, rule2, series)
+        }
+    }
 
     override fun generate(event: Event): List<Signal> {
         val result = mutableListOf<Signal>()
         val time = event.time.toUTC()
         for ((asset, price) in event.prices) {
             if (price is PriceBar) {
-                val (buyingRule, sellingRule, series) = data.getOrPut(asset) {
-                    val series = BaseBarSeriesBuilder().withName(asset.symbol).build()
-                    if (maxBarCount >= 0) series.maximumBarCount = maxBarCount
-                    val rule1 = buyingRule(series)
-                    val rule2 = sellingRule(series)
-                    Triple(rule1, rule2, series)
-                }
-
+                val (buyingRule, sellingRule, series) = getRules(asset)
                 series.addBar(time, price.open, price.high, price.low, price.close, price.volume)
                 if (buyingRule.isSatisfied(series.endIndex)) result.add(Signal(asset, Rating.BUY))
                 if (sellingRule.isSatisfied(series.endIndex)) result.add(Signal(asset, Rating.SELL))
@@ -52,22 +55,25 @@ class Ta4jStrategy(
     }
 
     /**
-     * Define the buy rule
+     * Define the buy [rule]
      */
-    fun buy(block: (series: BarSeries) -> Rule) {
-        buyingRule = block
+    fun buy(rule: (series: BarSeries) -> Rule) {
+        buyingRule = rule
     }
 
     /**
-     * Define the sell rule
+     * Define the sell [rule]
      */
-    fun sell(block: (series: BarSeries) -> Rule) {
-        sellingRule = block
+    fun sell(rule: (series: BarSeries) -> Rule) {
+        sellingRule = rule
     }
 
+    /**
+     * Reset this strategy and any series data captured so far.
+     */
     override fun reset() {
         super.reset()
-        data.clear()
+        rules.clear()
     }
 
 }
