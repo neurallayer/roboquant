7ba87475e24710ebabaf8c81dff72131d54d0e7a
Peter Dekkers <peter@jbaron.com>
2022-10-08 12:34:21 +0200

    improved random-walk feed by not having it all loaded in memory but still be reproducible.

--- roboquant/src/main/kotlin/org/roboquant/feeds/random/RandomWalk.kt
+++ roboquant/src/main/kotlin/org/roboquant/feeds/random/RandomWalk.kt
@@ -17,54 +17,95 @@
 package org.roboquant.feeds.random
 
 import org.roboquant.common.*
-import org.roboquant.feeds.HistoricPriceFeed
-import org.roboquant.feeds.PriceBar
-import org.roboquant.feeds.TradePrice
+import org.roboquant.feeds.*
 import java.time.Instant
 import java.time.LocalDate
-import kotlin.math.round
 import kotlin.random.Random
 import kotlin.random.asJavaRandom
 
 /**
- * Random walk creates a number of randomly named assets with a price history that follows a random walk using a
+ * Random walk creates a number of assets with a price history that follows a random walk using a
  * Normal distribution. It can be useful for testing, since if your strategy does well using this feed, there
  * might be something suspicious going on.
  *
- * It used a seeded random generator, so while is generates random data, the results can be reproduced if
- * instantiated with the same seed. It can generate both single prices and bar data. The data is all generated
- * upfront and stored in memory to ensure it is fully reproducible independent of the test scenario.
+ * Internally it uses a seeded random generator, so while is generates random data, the results can be reproduced if
+ * instantiated with the same seed. It can generate [PriceBar] or [TradePrice] prices.
  *
- * # Background
+ * ## Background
  * Random walk theory suggests that changes in stock prices have the same distribution and are independent of each
  * other. Therefore, it assumes the past movement or trend of a stock price or market cannot be used to predict its
  * future movement. In short, random walk theory proclaims that stocks take a random and unpredictable path that makes
  * all methods of predicting stock prices futile in the long run.
  *
- * @property timeline The timeline of this random walk.
+ * @property timeline the timeline of this random walk.
+ * @param nAssets the number of assets to generate
+ * @property generateBars should PriceBars be generated or not
+ * @property volumeRange
+ * @property priceRange
+ * @param template template to use when generating assets
+ * @property seed seed to use for initializing the random generator
  *
  */
 class RandomWalk(
-    timeline: Timeline,
+    override val timeline: Timeline,
     nAssets: Int = 10,
-    generateBars: Boolean = true,
-    minVolume: Int = 100_000,
-    maxVolume: Int = 1000_000,
-    maxDayRange: Double = 4.0,
-    symbolLength: Int = 4,
+    private val generateBars: Boolean = true,
+    private val volumeRange: Int = 1000,
+    private val priceRange: Double = 1.0,
     template: Asset = Asset("TEMPLATE"),
-    private val random: Random = Config.random
-) : HistoricPriceFeed() {
+    private val seed: Int = 42
+) : HistoricFeed {
+
+    override val assets = 1.rangeTo(nAssets).map { template.copy(symbol = "Asset-$it") }.toSortedSet()
 
     init {
-        repeat(nAssets) {
-            val symbol = generateSymbol(symbolLength)
-            val asset = template.copy(symbol = symbol)
+        logger.fine {
+            "Created $nAssets assets with ${timeline.size} events and a timeline " +
+                    "from ${timeline.first()} to ${timeline.last()}"
+        }
+    }
 
-            if (generateBars)
-                generateBars(asset, timeline, minVolume, maxVolume, maxDayRange)
-            else
-                generateSinglePrice(asset, timeline, minVolume, maxVolume)
+    private fun Random.firstPrice(): Double = nextDouble(50.0, 150.0)
+
+    private fun Random.nextPrice(price: Double) : Double = price + asJavaRandom().nextGaussian()
+
+    private fun Random.priceBar(asset: Asset, price: Double) : PriceAction {
+        val v = mutableListOf(price)
+        repeat(3) {
+            v.add(price + (nextDouble(-priceRange, priceRange)))
+        }
+        v.sort()
+        val volume = nextInt(volumeRange, volumeRange*2)
+        return if (nextBoolean()) {
+            PriceBar(asset, v[1], v[3], v[0], v[2], volume)
+        } else {
+            PriceBar(asset, v[2], v[3], v[0], v[1], volume)
+        }
+    }
+
+    /**
+     * Generate random single price actions
+     */
+    private fun Random.tradePrice(asset: Asset, price: Double) : PriceAction {
+        val volume = nextInt(volumeRange, volumeRange*2)
+        return TradePrice(asset, price, volume.toDouble())
+    }
+
+    override suspend fun play(channel: EventChannel) {
+        val prices = mutableMapOf<Asset, Double>()
+        val random = Random(seed)
+        for (time in timeline) {
+            val actions = mutableListOf<PriceAction>()
+            for (asset in assets) {
+                val lastPrice = prices[asset]
+                val price =  if (lastPrice == null) random.firstPrice() else random.nextPrice(lastPrice)
+                val action = if (generateBars) random.priceBar(asset, price) else random.tradePrice(asset, price)
+                actions.add(action)
+                prices[asset] = if (price < priceRange * 10) priceRange * 10 else price
+
+            }
+            val event = Event(actions, time)
+            channel.send(event)
         }
     }
 
@@ -87,78 +128,19 @@ class RandomWalk(
         /**
          * Create a random walk for the last [days], generating minute prices.
          */
-        fun lastDays(days: Int = 1, nAssets: Int = 10, generateBars: Boolean = true): RandomWalk {
+        fun lastDays(days: Int = 1, nAssets: Int = 10): RandomWalk {
             val last = Instant.now()
             val first = last - days.days
             val timeline = Timeframe(first, last).toTimeline(1.minutes)
-            return RandomWalk(timeline, nAssets, generateBars)
+            return RandomWalk(timeline, nAssets)
         }
 
     }
 
-    init {
-        logger.fine {
-            "Created $nAssets assets with ${timeline.size} events and a timeline " +
-                    "from ${timeline.first()} to ${timeline.last()}"
-        }
-    }
 
-    /**
-     * Generate random bars
-     */
-    private fun generateBars(
-        asset: Asset,
-        timeline: Timeline,
-        minVolume: Int,
-        maxVolume: Int,
-        maxDayRange: Double
-    ) {
-        var prevPrice = 100.0
-        val plusVolume = maxVolume - minVolume
-        val jRandom = random.asJavaRandom()
-        for (time in timeline) {
-            val newValue = jRandom.nextGaussian() + prevPrice
-            val v = mutableListOf(newValue)
-            repeat(3) {
-                v.add(newValue + (random.nextDouble() * maxDayRange) - (maxDayRange / 2.0f))
-            }
-            v.sort()
 
-            val volume = round(minVolume + (plusVolume * random.nextDouble()))
-            val action = if (random.nextBoolean()) {
-                PriceBar(asset, v[1], v[3], v[0], v[2], volume)
-            } else {
-                PriceBar(asset, v[2], v[3], v[0], v[1], volume)
-            }
-            add(time, action)
 
-            prevPrice = if (newValue > 10.0) newValue else 10.0
-        }
-    }
 
-    /**
-     * Generate random single price actions
-     */
-    private fun generateSinglePrice(asset: Asset, timeline: Timeline, minVolume: Int, maxVolume: Int) {
-        var prevPrice = 100.0
-        val plusVolume = maxVolume - minVolume
-        val jRandom = random.asJavaRandom()
-        for (time in timeline) {
-            val newValue = jRandom.nextGaussian() + prevPrice
-            val volume = round(minVolume + (plusVolume * random.nextDouble()))
-            val action = TradePrice(asset, newValue, volume)
-            add(time, action)
-            prevPrice = if (newValue > 10.0) newValue else 10.0
-        }
-    }
-
-    /**
-     * Generate a random symbol (ticker) name.
-     */
-    private fun generateSymbol(symbolLength: Int): String {
-        val alphabet = ('A'..'Z').toList()
-        return List(symbolLength) { alphabet.random(random) }.joinToString("")
-    }
 
 }
 
