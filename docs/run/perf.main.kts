dda4d548cb588a786b6924650f43dcfe85b50786
Peter Dekkers <peter@neurallayer.com>
2023-09-17 13:21:53 +0200

    prep for version 2.0

--- docs/run/perf.main.kts
+++ docs/run/perf.main.kts
@@ -14,6 +14,6 @@
  * limitations under the License.
  */
 
-@file:DependsOn("org.roboquant:roboquant-perf:2.0.0-SNAPSHOT")
+@file:DependsOn("org.roboquant:roboquant-perf:2.0.0")
 
 org.roboquant.perf.main()
