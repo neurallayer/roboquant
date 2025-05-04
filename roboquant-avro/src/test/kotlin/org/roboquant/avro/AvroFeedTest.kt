66e69e6085bf8d43f333433e3af18d488cabb33c
Peter Dekkers <peter@neurallayer.com>
2025-05-04 09:35:19 +0200

    updated license years

--- roboquant-avro/src/test/kotlin/org/roboquant/avro/AvroFeedTest.kt
+++ roboquant-avro/src/test/kotlin/org/roboquant/avro/AvroFeedTest.kt
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2020-2024 Neural Layer
+ * Copyright 2020-2025 Neural Layer
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
