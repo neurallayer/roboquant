f8606772c6c35b7e19912458fdd81a437b3f881f
Peter Dekkers <peter@neurallayer.com>
2025-05-05 09:59:13 +0200

    updated packages 2

---
+++ roboquant/src/main/kotlin/org/roboquant/common/Account.kt
@@ -0,0 +1,104 @@
+package org.roboquant.common
+
+import java.time.Instant
+import kotlin.collections.iterator
+
+/**
+ * An account represents a brokerage trading account and is unified across all broker implementations.
+ * It holds the following state:
+ *
+ * - The buying power
+ * - The base currency
+ * - All the [cash] balances in the account
+ * - All the open [positions] with its assets
+ * - The [orders]
+ *
+ * Some convenience methods convert a multi-currency Wallet to a single-currency Amount.
+ * For this to work, you'll need to have the appropriate exchange rates defined
+ * at [Config.exchangeRates].
+ *
+ * @property baseCurrency the base currency of the account
+ * @property lastUpdate time that the account was last updated
+ * @property cash cash balances
+ * @property orders List of [Order] of all open orders
+ * @property positions List of all open [Position], with maximum one entry per asset
+ * @property buyingPower amount of buying power available for trading
+ * @constructor Create a new Account
+ */
+interface Account {
+
+    val baseCurrency: Currency
+    val lastUpdate: Instant
+    val cash: Wallet
+    val orders: List<Order>
+    val positions: Map<Asset, Position>
+    val buyingPower: Amount
+
+    /**
+     * Cash balances converted to a single amount denoted in the [baseCurrency] of the account. If you want to know
+     * how much cash is available for trading, please use [buyingPower] instead.
+     */
+    val cashAmount: Amount
+        get() = convert(cash)
+
+    /**
+     * [equity] converted to a single amount denoted in the [baseCurrency] of the account.
+     */
+    fun equityAmount(): Amount = convert(equity())
+
+    /**
+     * Calculate the total equity hold in the account and return the result as a [Wallet]
+     * Equity is defined as the sum of [cash] balances and the market value of the open [positions].
+     */
+    fun equity(): Wallet = cash + marketValue()
+
+    /**
+     * The unique set of assets hold in the open [positions]
+     */
+    val assets: Set<Asset>
+        get() = positions.keys
+
+    /**
+     * Return the market value of the open positions, optionally filter by one or more asset.
+     */
+    fun marketValue(vararg assets: Asset): Wallet {
+        val v = positions.filterKeys { assets.isEmpty() || it in assets }
+        val result = Wallet()
+        for ((asset, position) in v) {
+            val positionValue = asset.value(position.size, position.mktPrice)
+            result.deposit(positionValue)
+        }
+        return result
+    }
+
+    /**
+     * Return the position size for the provided asset within this account. If there is no open position,
+     * a size of ZERO is returned.
+     */
+    fun positionSize(asset: Asset) : Size = positions[asset]?.size ?: Size.ZERO
+
+    /**
+     * Return the unrealized PNL of the open positions, optionally filter by one or more asset. If there is
+     * not match, an empty [Wallet] will be returned.
+     */
+    fun unrealizedPNL(vararg assets: Asset): Wallet {
+        val v = positions.filterKeys { assets.isEmpty() || it in assets }
+        val result = Wallet()
+        for ((asset, position) in v) {
+            val positionValue = asset.value(position.size, position.mktPrice - position.avgPrice)
+            result.deposit(positionValue)
+        }
+        return result
+    }
+
+    /**
+     * Convert an [amount] to the account [baseCurrency] using last update of the account as a timestamp
+     */
+    fun convert(amount: Amount): Amount = amount.convert(baseCurrency, lastUpdate)
+
+    /**
+     * Convert a [wallet] to the account [baseCurrency] using last update of the account as a timestamp
+     */
+    fun convert(wallet: Wallet): Amount = wallet.convert(baseCurrency, lastUpdate)
+
+}
---
+++ roboquant/src/main/kotlin/org/roboquant/common/Event.kt
@@ -0,0 +1,72 @@
+package org.roboquant.common
+
+import java.time.Instant
+
+/**
+ * An event contains a list of [items] that all happened at the same moment in [time]. An [Item]
+ * can be anything, but a common use case is price items like candlesticks.
+ *
+ * @property items the list of actions that are part of this event
+ * @property time the time that the actions in this event became available
+ */
+class Event(val time: Instant, val items: List<Item>) : Comparable<Event> {
+
+    /**
+     * Convenience property for accessing the price actions in this event. The result is cached so that accessing
+     * this property multiple times is quick.
+     *
+     * If there are multiple price actions for a single asset in the event, the last one found will be returned. If
+     * you need access to all prices for an asset, iterate over the [items] directly.
+     */
+    val prices: Map<Asset, PriceItem> by lazy {
+        val result = HashMap<Asset, PriceItem>(items.size)
+        for (action in items) if (action is PriceItem) result[action.asset] = action
+        result
+    }
+
+    /**
+     * @suppress
+     */
+    companion object {
+
+        /**
+         * Return an event without any [items] with as default [time] the current system time.
+         */
+        fun empty(time: Instant = Instant.now()): Event = Event(time, emptyList())
+
+    }
+
+    /**
+     * Convenience method to get a single price for an [asset] or null if there is no price action present for
+     * the asset in this event. Optionally you can specify the [type] of price.
+     *
+     * If there are multiple price actions for a single asset in the event, the last one found will be returned. If
+     * you require access to all prices for an asset, access [items] directly.
+     */
+    fun getPrice(asset: Asset, type: String = "DEFAULT"): Double? {
+        return prices[asset]?.getPrice(type)
+    }
+
+    /**
+     * Compare this event to an [other] event based on their [time]. This is used for sorting a list of events by
+     * their chronological order.
+     */
+    override fun compareTo(other: Event): Int = time.compareTo(other.time)
+
+    /**
+     * Return true if this is event has at least one action, false otherwise
+     */
+    fun isNotEmpty(): Boolean = items.isNotEmpty()
+
+    /**
+     * Return true if this event has no actions, false otherwise
+     */
+    fun isEmpty(): Boolean = items.isEmpty()
+
+    /**
+     * Provide the event time
+     */
+    override fun toString(): String {
+        return "Event(time=$time actions=${items.size})"
+    }
+}
